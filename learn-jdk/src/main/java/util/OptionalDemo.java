17e9e41ed9ea1411103ca71ced33b0c45fb8876e
splade <splade2009@gmail.com>
2017-12-11 23:34:21 +0800

    学习中 ...

--- learn-jdk/src/main/java/util/OptionalDemo.java
+++ learn-jdk/src/main/java/util/OptionalDemo.java
@@ -1,7 +1,9 @@
 package util;
 
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import java.util.function.Function;
+import java.util.function.Predicate;
 
 public class OptionalDemo {
 
@@ -10,6 +12,45 @@ public class OptionalDemo {
 
         Optional<Double> d = Optional.empty();
         d = Optional.of(3.14);
+        d = Optional.ofNullable(null);
+
+        d.isPresent();
+        if (d.isPresent())
+            d.get(); // 如果没有值,会抛出 NoSuchElementException
+
+        d.orElse(0.0);
+        d.orElseGet(() -> 3.14);
+        try {
+            d.orElseThrow(() -> new NoSuchElementException("Exception"));
+        } catch (NoSuchElementException e) {
+            System.out.println(e);
+        }
+
+
+        d.ifPresent((value) -> System.out.println(value));
+        d.map(new Function<Double, String>() {
+            @Override
+            public String apply(Double value) {
+                return "{value: " + value + "}";
+            }
+        });
+
+        d.filter(new Predicate<Double>() {
+            @Override
+            public boolean test(Double value) {
+                if (value > 0.5)
+                    return false;
+                else
+                    return true;
+            }
+        });
+
+        d.flatMap(new Function<Double, Optional<String>>() {
+            @Override
+            public Optional<String> apply(Double value) {
+                return Optional.ofNullable(String.valueOf(value));
+            }
+        });
 
         System.out.println(d);
 
